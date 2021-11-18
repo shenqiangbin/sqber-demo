f1f2d5e9faa8d583e46e537b577f1f6535c60279
shenqiangbin <1348907384@qq.com>
2021-11-18 08:52:08 +0800

    feat: 使用 @ComponentScan 的另一种写法

--- spring-log4j-demo/src/main/java/com/sqber/log4jdemo/Log4jDemoApplication.java
+++ spring-log4j-demo/src/main/java/com/sqber/log4jdemo/Log4jDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.sqber.log4jdemo, com.sqber.commonWeb")
+@ComponentScan({"com.sqber.log4jdemo", "com.sqber.commonWeb"})
 public class Log4jDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(Log4jDemoApplication.class, args);
