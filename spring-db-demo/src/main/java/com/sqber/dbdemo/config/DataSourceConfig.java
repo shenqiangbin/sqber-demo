3f06450f58ce509e93818b408f71b0f0382d3896
shenqiangbin <1348907384@qq.com>
2021-11-21 08:59:13 +0800

    Feat: 多数据源测试

--- spring-db-demo/src/main/java/com/sqber/dbdemo/config/DataSourceConfig.java
+++ spring-db-demo/src/main/java/com/sqber/dbdemo/config/DataSourceConfig.java
@@ -2,6 +2,9 @@ package com.sqber.dbdemo.config;
 
 import com.sqber.commonTool.db.MyJdbc;
 import com.sqber.commonTool.db.impl.MyJdbcTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -16,4 +19,24 @@ public class DataSourceConfig {
     public MyJdbc primaryJdbc(DataSource dataSource) {
         return new MyJdbcTemplate(dataSource);
     }
+
+    @Bean("dataMyJdbc")
+    public MyJdbc dataMyJdbc(@Qualifier("dataDataSource") DataSource dataSource) {
+        return new MyJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.userdb") // application.properteis中对应属性的前缀
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("dataDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.datadb")
+    public DataSource dataSource2() {
+        return DataSourceBuilder.create().build();
+    }
+
+
 }
